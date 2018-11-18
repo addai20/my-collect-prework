2aea38e74237d59d6cf84a89c0416a1dcf2749da
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-18 01:26:10 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,25 +1,5 @@
 def my_collect(collection)
-  new_collection = []
-  
-  if collection.length == 0 
-    break
-  end
-  
-  i= 0
-  
-  #If collection elements are languages... do the following!
-  if collection[i].split(" ").length == 1
-    while i < collection.length 
-      yield new_collection << collection[i].capitalize
-      i+=1
-      end
-  #If collection elements are names (first and last)... do the following!   
-  else
-    while i < collection.length
-    yield new_collection << collection[i].first
-    end
-end
-return new_collection
+ 
 end
 
 
