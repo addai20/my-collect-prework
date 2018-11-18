9cb6f4e58b26c59ff073a75858fdbf87df3c1946
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-18 01:28:49 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -6,16 +6,16 @@ end
 
 # https://stackoverflow.com/questions/5347949/whats-different-between-each-and-collect-method-in-ruby
 
-Array#each takes an array and applies the given block over all items. It doesn't affect the array or creates a new object. It is just a way of looping over items. Also it returns self.
+# Array#each takes an array and applies the given block over all items. It doesn't affect the array or creates a new object. It is just a way of looping over items. Also it returns self.
 
-  arr=[1,2,3,4]
-  arr.each {|x| puts x*2}
-Prints 2,4,6,8 and returns [1,2,3,4] no matter what
+#   arr=[1,2,3,4]
+#   arr.each {|x| puts x*2}
+# Prints 2,4,6,8 and returns [1,2,3,4] no matter what
 
-Array#collect is same as Array#map and it applies the given block of code on all the items and returns the new array. simply put 'Projects each element of a sequence into a new form'
+# Array#collect is same as Array#map and it applies the given block of code on all the items and returns the new array. simply put 'Projects each element of a sequence into a new form'
 
-  arr.collect {|x| x*2}
-Returns [2,4,6,8]
+#   arr.collect {|x| x*2}
+# Returns [2,4,6,8]
 
 
 
